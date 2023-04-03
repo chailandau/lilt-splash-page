2aa1b8158b53aebeda1193b9e9ed4cf963c71869
Chai Landau <chai.landau@gmail.com>
2023-04-03 14:58:18 -0400

    Added FtImages content

--- src/components/FtImages.tsx
+++ src/components/FtImages.tsx
@@ -5,6 +5,12 @@ const FtImages = () => {
     return (
         <div id='ft-images'>
             <Wrapper>
+                <div className="image blue-glow-border">
+                    <Image src='/images/splash-img-1.jpg' alt="Outdoor laser tag tents" fill />
+                </div>
+                <div className="image blue-glow-border">
+                    <Image src='/images/splash-img-2.jpg' alt="Group of kids holding laser tag equipment" fill />
+                </div>
             </Wrapper>
         </div>
     )
